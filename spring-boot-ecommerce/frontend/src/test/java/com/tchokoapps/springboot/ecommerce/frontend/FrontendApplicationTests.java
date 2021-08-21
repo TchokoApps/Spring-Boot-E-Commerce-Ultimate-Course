51d792f7639388eb1ce74e54f16879396e9a9d13
TchokoApps <tchokoapps@gmail.com>
2021-08-21 16:38:24 +0200

    update

---
+++ spring-boot-ecommerce/frontend/src/test/java/com/tchokoapps/springboot/ecommerce/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchokoapps.springboot.ecommerce.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
